22a32ac9945438429eaad2cdc00b02717c5e26bf
mvatanasovFMI <mihail.atanasov@linkmobility.com>
2020-03-16 00:22:05 +0200

    Updates

---
+++ precache-manifest.7b1537cd00accf1ea20edaa6e86ff9d6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a41c15c9d9e92894d06198e63e2734a8",
+    "url": "/holiday-extra/index.html"
+  },
+  {
+    "revision": "92b41b553578b5046ec5",
+    "url": "/holiday-extra/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0f1ed2d14cf9d0a304f1",
+    "url": "/holiday-extra/static/js/2.d3e960eb.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/holiday-extra/static/js/2.d3e960eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92b41b553578b5046ec5",
+    "url": "/holiday-extra/static/js/main.94cf373d.chunk.js"
+  },
+  {
+    "revision": "a317888363dfb6da488d",
+    "url": "/holiday-extra/static/js/runtime-main.825a36e2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/holiday-extra/static/media/logo.5d5d9eef.svg"
+  }
+]);
